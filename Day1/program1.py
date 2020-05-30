e9f21c8323f3db4c061dde3f0aa806f2226645d3
Pradyumn Pal <66147815+pradyumn-pal@users.noreply.github.com>
2020-05-30 06:42:11 +0530

    Create program1.py

---
+++ Day1/program1.py
@@ -0,0 +1,12 @@
+#solution of day1/program1
+
+name = input("Enter your Name: ")
+branch = input("Tell me your Branch: ")
+gender = input("What is your Gender: ")
+college_Name = input("What's your College Name: ")
+age = int(input("Enter your Age: "))
+print("Name:",name)
+print("Branch:",branch)
+print("Gender:",gender)
+print("College Name:",college_Name)
+print("Age:",age)
