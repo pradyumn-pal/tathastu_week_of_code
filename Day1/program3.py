6b9a8d51dba9283c26dac1f75310db428108e0a1
Pradyumn Pal <66147815+pradyumn-pal@users.noreply.github.com>
2020-05-31 17:02:59 +0530

    Create program3.py

---
+++ Day1/program3.py
@@ -0,0 +1,9 @@
+# swapping two number using third variable
+a=int(input("enter the value of a:")
+b=int(input("enter the value of b:")
+a=a+b
+b=a-b
+a=a-b
+print("after swapping")
+print("value of a=",b)
+print("value of b=",b)
