9690592f206679657fc53b801a8aa9343c7073a9
Pradyumn Pal <66147815+pradyumn-pal@users.noreply.github.com>
2020-05-31 17:09:36 +0530

    Create program4.py

---
+++ Day1/program4.py
@@ -0,0 +1,6 @@
+Cp=float (input ("Enter cost price "))            #Cp=cost price
+Sp=float (input ("Enter selling  price "))        #Sp=selling price
+Profit=float (Sp-Cp)
+Sp=Sp+(Profit *.05)
+print ("Profit from this sell",Profit )
+print ("New selling price will be ",Sp)
